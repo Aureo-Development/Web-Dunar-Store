d08eb1ab8e274e30448aa33ff2fc8395e61a2763
DanteJRFL <dantejrfl@gmail.com>
2026-08-19 14:28:42 -0300

    Alterações de Imagem e Header Responsivo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
 import { FaBars } from "react-icons/fa"
-import { FaUser } from "react-icons/fa"
 import { TiShoppingCart } from "react-icons/ti"
 import './App.css'
 
 function App() {
   const [menuAberto, setMenuAberto] = useState(false)
-
-
   return(
-    <div id="header">
+    <><div id="header">
       <div id="menu">
-        <button id="menu-icon" onClick={() => setMenuAberto(!menuAberto)}><FaBars/></button>
+        <button id="menu-icon" onClick={() => setMenuAberto(!menuAberto)}><FaBars /></button>
         {/* <nav className={menuAberto ? 'menu aberto' : 'menu'}>
-          <li><a href="/products">Products</a></li>
-          <li><a href="/contact">Contact</a></li>
-          <li><a href="/about">About</a></li>
-        </nav> */}
+      <li><a href="/products">Products</a></li>
+      <li><a href="/contact">Contact</a></li>
+      <li><a href="/about">About</a></li>
+    </nav> */}
       </div>
       <div id="logo">
-        <img id="logo-img-header" src="/src/assets/DUNAR CARIMBO.png" alt="LOGO DUNAR"/>
+        <img id="logo-img-header" src="/src/assets/DUNAR LOGO (nbg).png" alt="LOGO DUNAR" />
       </div>
       <nav>
-        <button id="user-icon"><FaUser/></button>
-        <button id="cart-icon"><TiShoppingCart/></button>
+        <button id="cart-icon"><TiShoppingCart /></button>
       </nav>
-
-  
     </div>
+    <div className="carrossel">
+      <div className="trilha">
+          
+      </div>
+    </div>
+    </>
   )
 }
-
 export default App
